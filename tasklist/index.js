6c356888c4e5426476f163c17c0a4709427ba40e
GAURAV <gauravpatil8433@gmail.com>
2020-08-22 17:44:15 +0530

    list added

---
+++ tasklist/index.js
@@ -0,0 +1,27 @@
+
+
+
+document.querySelectorAll(".checkbox").forEach(item =>{
+    item.addEventListener("click",()=>{
+        item.classList.add("checkeddisplay");
+        function checked(){
+            item.classList.add("checked");
+        }
+        setTimeout(checked, 150);
+        function remove(){
+            item.parentElement.style.left = "50vw";
+        }
+        setTimeout(remove, 150);
+        function fade(){
+            item.parentElement.style.display = "none";
+        }
+        setTimeout(fade, 400);
+        
+        
+    });
+});
+
+
+
+
+
